2bcdadb32ba88dfeca40979fc0b6cc3692c21a97
Siamak Dehghan <siamak.dehghan@tpk.govt.nz>
2024-09-03 13:19:57 +1200

    Date update

--- script/script.js
+++ script/script.js
@@ -13,7 +13,7 @@
   			localLinks[i].setAttribute('target', '_blank');
   		}
   		document.querySelector(".main-container")
-  			.insertAdjacentHTML('afterend', '<div id="footer" style="left:0;bottom:0;width:calc(100% - 15px)"><h6 style="background-color:#333333;color:white;margin-top: 3px;margin-bottom: 0px;line-height: 60px;margin-left: 0px;margin-right: -15px;padding-left: 15px;font-size: 0.8em;"> Te Puni Kōkiri <span style="color:gray"> Te Puni Whakahaere Tikanga, <a href="mailto:matawhanui@tpk.govt.nz" style="color:gray">Data & Insights</a> - May 2024 </span><a href="https://www.tpk.govt.nz/en/o-matou-mohiotanga/climate" target="_blank"><img id="footer-img" src="img/footer_logo.svg" style="height:44px;float:right;margin-top:8px;margin-right:15px;" /></a></h6></div>');
+  			.insertAdjacentHTML('afterend', '<div id="footer" style="left:0;bottom:0;width:calc(100% - 15px)"><h6 style="background-color:#333333;color:white;margin-top: 3px;margin-bottom: 0px;line-height: 60px;margin-left: 0px;margin-right: -15px;padding-left: 15px;font-size: 0.8em;"> Te Puni Kōkiri <span style="color:gray"> Te Puni Whakahaere Tikanga, <a href="mailto:matawhanui@tpk.govt.nz" style="color:gray">Data & Insights</a> - Sep 2024 </span><a href="https://www.tpk.govt.nz/en/o-matou-mohiotanga/climate" target="_blank"><img id="footer-img" src="img/footer_logo.svg" style="height:44px;float:right;margin-top:8px;margin-right:15px;" /></a></h6></div>');
   	}, 20);
   });
 </script>
